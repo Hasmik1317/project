07ee1d30ddec975b6153feec84bae75d02306d0c
Hasmik1317 <danielyanhas81@gmail.com>
2025-06-23 01:05:20 +0400

    networkscan.py

--- networkscan.py
+++ networkscan.py
@@ -3,35 +3,30 @@ import argparse
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--target', dest='target', help='Target IP Address/Adresses')
+    parser.add_argument('-t', '--target', dest='target', help='Target IP Address or Range')
     options = parser.parse_args()
 
-    
     if not options.target:
-       
-        parser.error("[-] Please specify an IP Address or Addresses, use --help for more info.")
+        parser.error("[-] Please specify an IP address or range, use --help for more info.")
     return options
-  
+
 def scan(ip):
-    arp_req_frame = scapy.ARP(pdst = ip)
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-    broadcast_ether_frame = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
-    
-    broadcast_ether_arp_req_frame = broadcast_ether_frame / arp_req_frame
-
-    answered_list = scapy.srp(broadcast_ether_arp_req_frame, timeout = 1, verbose = False)[0]
     result = []
-    for i in range(0,len(answered_list)):
-        client_dict = {"ip" : answered_list[i][1].psrc, "mac" : answered_list[i][1].hwsrc}
-        result.append(client_dict)
-
+    for sent, received in answered_list:
+        client = {"ip": received.psrc, "mac": received.hwsrc}
+        result.append(client)
     return result
-  
+
 def display_result(result):
-    print("\nIP Address\tMAC Address\n")
-    for i in result:
-        print("{}\t{}".format(i["ip"], i["mac"]))
-  
+    print("\nIP Address\t\tMAC Address")
+    print("-" * 40)
+    for client in result:
+        print(f"{client['ip']}\t\t{client['mac']}")
 
 options = get_args()
 scanned_output = scan(options.target)
